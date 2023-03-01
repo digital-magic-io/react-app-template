161ca2b793e5a710dc445694b08f450a0e75fc55
jgoloborodko <jevgeni@digital-magic.io>
2023-03-01 19:16:00 +0200

    Fixed issue where translation didn't change for form field errors.

--- src/components/controls/Form/FormCheckbox.tsx
+++ src/components/controls/Form/FormCheckbox.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react'
 import { Controller, useFormContext } from 'react-hook-form'
-import { FormControl, FormHelperText } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+import { getPath } from 'ts-object-path'
 import { hasValue } from '@digital-magic/ts-common-utils'
+import { propertyKeysToPath } from '@digital-magic/react-common/lib/components/controls/form/utils'
+import { FormControl, FormHelperText } from '@mui/material'
 import { Checkbox, CheckboxProps } from '../Checkbox'
 import { useFormInputProps, FormInputProps } from './'
 
@@ -16,6 +19,15 @@ export const FormCheckbox: React.FC<Props> = ({ className, ...props }) => {
   const f = useFormContext()
   const { name, error, helperText } = useFormInputProps(props)
   const { hideErrorMessage, ...inputProps } = props
+  const { i18n } = useTranslation()
+
+  React.useEffect(() => {
+    const name = propertyKeysToPath(getPath(props.name))
+    if (f.getFieldState(name).isDirty && hasValue(error)) {
+      void f.trigger(name)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [i18n.language])
 
   return (
     <FormControl component="fieldset" error={error} className={className}>
